3900249b2d4e4dabddabbc7d4f7d594e1e142992
ZheHong1021 <zhehome1021@gmail.com>
2024-06-21 23:43:17 +0800

    feat: 將SwaggerUI增加可以使用 form/data POST功能

--- config/settings/swagger.py
+++ config/settings/swagger.py
@@ -2,4 +2,20 @@
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Django Project",
+
+    # 描述
+    "DESCRIPTION": "這是一個測試API的平台",
+
+    # 版本
+    "VERSION": "1.0.0", 
+    
+    "SERVE_INCLUDE_SCHEMA": False,
+
+    # 確保可以使用 form/data
+    "POST_PROCESSING_HOOKS": [
+        "drf_spectacular.hooks.postprocess_schema_enums",
+        "drf_spectacular.hooks.postprocess_schema_customization"
+    ],
+    
+    "COMPONENT_SPLIT_REQUEST": True,  
 }
