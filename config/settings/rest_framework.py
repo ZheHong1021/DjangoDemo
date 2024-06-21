df322b29031f94b948d152c6d2271de540be6395
zhe-hong-Lin <58475836+ZheHong1021@users.noreply.github.com>
2024-06-22 00:13:30 +0800

    feat: Menu添加filter + 設定DRF全域相關實用設定 (#9)

--- config/settings/rest_framework.py
+++ config/settings/rest_framework.py
@@ -11,4 +11,21 @@ REST_FRAMEWORK = {
     
     # Swagger-UI
     'DEFAULT_SCHEMA_CLASS': "drf_spectacular.openapi.AutoSchema",
+    
+
+    # Filter Backend
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter'
+    ], 
+
+    # 設定 ordering使用時的參數
+    'ORDERING_PARAM': 'ordering',  # 排序參數
+
+
+    # 【換頁限制】
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+
+    # 日期時間格式
+    'DATETIME_FORMAT': "%Y-%m-%d %H:%M:%S",
 }
--- menus/views.py
+++ menus/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Menu
 from .serializers import MenuSerializer
+from .filters import MenuFilter
 
 class MenuViewSet(viewsets.ModelViewSet):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    filterset_class = MenuFilter
