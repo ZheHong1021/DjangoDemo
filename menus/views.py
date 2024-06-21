feb7f1b846ca76c9511aea9a794f6ef947abea3e
ZheHong1021 <zhehome1021@gmail.com>
2024-06-22 00:16:29 +0800

    feat: menu添加CustomPagination可以進行分頁功能

--- menus/views.py
+++ menus/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 from .models import Menu
 from .serializers import MenuSerializer
 from .filters import MenuFilter
+from common.paginations import CustomPagination
 
 class MenuViewSet(viewsets.ModelViewSet):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     filterset_class = MenuFilter
+    pagination_class = CustomPagination
