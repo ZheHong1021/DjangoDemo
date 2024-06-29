dbe696a07c7bd611a9e7dac0d9623c224227011a
ZheHong1021 <zhehome1021@gmail.com>
2024-06-30 02:05:28 +0800

    feat: 添加創建用戶進行密碼加密

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,31 @@ from .models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+    # 全名
+    fullname = serializers.SerializerMethodField()
+
+    # 性別顯示
+    gender_display = serializers.SerializerMethodField()
+    
     class Meta:
         model = CustomUser
         fields = "__all__"
+        # password只允許寫入
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def get_fullname(self, instance):
+        lastname = instance.lastname or ''
+        firstname = instance.firstname or ''
+        return lastname + firstname
+
+    def get_gender_display(self, instance):
+        return instance.get_gender_display()
+
+    def create(self, validated_data):
+        password = validated_data.pop('password') # 將密碼區隔開來
+        user = CustomUser(**validated_data)
+        user.set_password(password) # 再最後設定密碼並且儲存
+        user.save()
+        return user
