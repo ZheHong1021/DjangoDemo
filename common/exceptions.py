79483b086c5b58b0a163406bf5e8533cbb6bdcc1
ZheHong1021 <zhehome1021@gmail.com>
2024-06-29 10:40:52 +0800

    feat: 將Group APP中添加 exceptions並且應運

--- common/exceptions.py
+++ common/exceptions.py
@@ -8,5 +8,18 @@ def custom_exception_handler(exc, context):
     # Now add the HTTP status code to the response.
     if response is not None:
         response.data['status_code'] = response.status_code
-
     return response
+
+
+
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+class BaseException(APIException):
+    detail = None
+    status_code = None
+
+    def __init__(self, detail, code):
+        super().__init__(detail, code)
+        self.detail = detail
+        self.status_code = code
