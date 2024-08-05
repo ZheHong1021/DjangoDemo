c759a417d08b85b41c343d905afb64c171c55758
ZheHong1021 <zhehome1021@gmail.com>
2024-08-05 22:56:00 +0800

    feat: 將目前有使用的API添加SwaggerUI

--- common/views.py
+++ common/views.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers, viewsets
 
+
 # [創建時] 儲存使用者ID
 class CreateWithUserMixin:
     def perform_create(self, serializer):
@@ -21,3 +22,83 @@ class SoftDeleteModelViewSet(viewsets.ModelViewSet):
         # Soft delete the instance
         instance.is_deleted = True
         instance.save()
+
+
+
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiRequest, OpenApiResponse
+from drf_spectacular.types import OpenApiTypes
+
+# [Swagger UI For ModelViewSet]
+class SwaggerSchemaMixin:
+    model_class = None
+    serializer_class = None
+    list_parameters = []
+    list_summary = "得到所有數據"
+    detail_summary = "得到特定ID的數據"
+    create_summary = "創建數據"
+    update_summary = "修改數據"
+    delete_summary = "刪除數據"
+
+    #region (List)
+    @extend_schema(
+        summary=list_summary,
+        responses={
+            200: OpenApiResponse(response=OpenApiTypes.OBJECT, description="成功返回數據"),
+            401: OpenApiResponse(description="未授權"),
+            403: OpenApiResponse(description="禁止訪問"),
+        }
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    #endregion
+
+    #region (retrieve)
+    @extend_schema(
+        summary=detail_summary,
+        responses={
+            200: OpenApiResponse(response=OpenApiTypes.OBJECT, description="成功返回特定數據"),
+            404: OpenApiResponse(description="找不到為該ID的數據"),
+        }
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    #endregion
+
+
+    #region (create)
+    @extend_schema(
+        summary=create_summary,
+        responses={
+            201: OpenApiResponse(response=OpenApiTypes.OBJECT, description="成功創建數據"),
+            400: OpenApiResponse(description="無效的請求"),
+        }
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    #endregion
+
+    #region (update)
+    @extend_schema(
+        summary=update_summary,
+        # request=OpenApiTypes.OBJECT,
+        responses={
+            200: OpenApiResponse(response=OpenApiTypes.OBJECT, description="成功修改數據"),
+            400: OpenApiResponse(description="無效的請求"),
+            404: OpenApiResponse(description="找不到為該ID的數據"),
+        }
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    #endregion
+
+    #region (delete)
+    @extend_schema(
+        summary=delete_summary,
+        responses={
+            204: OpenApiResponse(description="成功刪除數據"),
+            404: OpenApiResponse(description="找不到為該ID的數據"),
+        }
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+    #endregion
--- jwt_auth_token/urls.py
+++ jwt_auth_token/urls.py
@@ -1,12 +1,13 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenVerifyView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenVerifyView,
+#     TokenRefreshView,
+# )
+from .views import *
 from django.urls import path, include
 
 urlpatterns = [
-    path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
+    path('refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
